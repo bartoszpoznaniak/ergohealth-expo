f4240fbf53186f9cc230d56530a8677f0c2be749
Bartosz Poznananiak <bartek.poznaniak@gmail.com>
2025-04-15 17:11:42 +0200

    add initial screen

---
+++ app/connect/index.tsx
@@ -0,0 +1,10 @@
+import { Button, Text, View } from "react-native";
+
+export function Connect() {
+    return (
+        <View>
+            <Text>Connect</Text>
+            <Button title="Connect" onPress={() => { }} />
+        </View>
+    )
+}
